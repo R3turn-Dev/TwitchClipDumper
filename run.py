f9c1dcb9950463da51cbde28b4a64340c15c99cc
EunhakLee <admin@return0927.xyz>
2019-03-10 01:59:03 +0900

    Bulk-Updated program
    - reformatted as PEP
    - Fixed #2 : changed filename-escaping method from replacing into numeric + allowed characters
    - Fixed #3 : added content validation checking

--- run.py
+++ run.py
@@ -2,12 +2,15 @@ import os
 import argparse
 import requests
 import pickle
+import traceback
 from datetime import datetime
 
 parser = argparse.ArgumentParser(description="Download Twitch clips.")
 parser.add_argument("channel", nargs="?", help="Channel ID")
 parser.add_argument("--client-id", nargs="?", help="Client ID")
 parser.add_argument("--token", nargs="?", help="OAuth2 Authorization token")
+
+VALID_FILENAME = lambda s: "".join(x for x in s if (x.isalnum() or x in "~!@#$%^&()_-+=[]{}.,"))
 
 
 def main(channel, client_id, token):
@@ -21,56 +24,45 @@ def main(channel, client_id, token):
         clips += _data
 
     print(len(clips), "clips found.")
-    pickle.dump(clips, open(f"clips-{channel}.pkl", "wb"))
-    print(f"Exported clips data as a pickle file 'clips-{channel}.pkl'")
 
-    Downloading = [
+    pickle_name = f"clips-{channel}.pkl"
+    pickle.dump(clips, open(pickle_name, "wb"))
+    print(f"Exported clips data as a pickle file '{pickle_name}'")
+
+    downloading = [
         (
             c['title'],
             int(datetime.strptime(c['created_at'], "%Y-%m-%dT%H:%M:%SZ").timestamp()),
             c['curator']['name'] if c['curator']['name'] == c['curator']['display_name'] else
-                c['curator']['display_name'] + f"({c['curator']['name']})",
+            c['curator']['display_name'] + f"({c['curator']['name']})",
             f"https://clips-media-assets2.twitch.tv/AT-cm%7C{c['tracking_id']}.mp4"
-         ) for c in clips
+        ) for c in clips
     ]
 
     open(f"urls-{channel}.txt", "w", encoding="UTF-8").write(
         "\n".join([
-            f"{channel}-{c[0]}-{c[1]}.mp4".replace("\\", "_")
-                .replace("/", "_")
-                .replace(":", "_")
-                .replace("*", "_")
-                .replace("\"", "_")
-                .replace("?", "_")
-                .replace("<", "_")
-                .replace(">", "_")
-                .replace("|", "_") for c in Downloading
+            VALID_FILENAME(f"{channel}-{c[0]}-{c[1]}.mp4") for c in downloading
         ])
     )
 
     if not channel in os.listdir(os.getcwd()):
         os.mkdir(channel)
 
-    for title, time, naming, uri in Downloading:
+    for title, time, naming, uri in downloading:
         try:
-            fname = f"{title}-{naming}.mp4"\
-                .replace("\\", "_")\
-                .replace("/", "_")\
-                .replace(":", "_")\
-                .replace("*", "_")\
-                .replace("\"", "_")\
-                .replace("?", "_")\
-                .replace("<", "_")\
-                .replace(">", "_")\
-                .replace("|", "_")
+            fname = VALID_FILENAME(f"{title}-{naming}.mp4")
 
-            open(f"{channel}/{fname}", "wb").write(
-                requests.get(uri).content
-            )
+            req = requests.get(uri)
+            if req.content:
+                open(f"{channel}/{fname}", "wb").write(req.content)
 
-            os.utime(f"{channel}/{fname}", (time, time))
+                os.utime(f"{channel}/{fname}", (time, time))
+
+            else:
+                print("Empty response", title, naming, uri)
+
         except:
-            print("Error on", title, naming, uri)
+            print("Error on", title, naming, uri, traceback.format_exc())
 
 
 def fetcher(channel, client_id, token, pagination: str = None):
@@ -80,7 +72,7 @@ def fetcher(channel, client_id, token, pagination: str = None):
         ,
         headers={
             "Accept": "vnd.twitchtv.v5+json",
-            "Authorization": "Bearer "+token,
+            "Authorization": "Bearer " + token,
             "Client-ID": client_id
         }
     )
